8473b5e4f127aaeaed8bb2b51f6d16b0b6fd8d14
Efron Paduansi <eufrondpaduansi@gmail.com>
2021-01-24 20:54:18 +0700

    Add files via upload
    Upload Project UTS Semester 5

---
+++ utsweb/functions.php
@@ -0,0 +1,33 @@
+<?php 
+
+//Koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "DbWeb");
+
+function registrasi($data){
+
+	global $conn;
+
+	$fullname = $data["fullname"];
+	$username = strtolower(stripcslashes($data["username"]));
+	$password = mysqli_real_escape_string($conn, $data["password"]);
+	$password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+	//cek konfirmasi password
+	if( $password !== $password2) {
+		echo "<script>
+					alert('konfirmasi Password Salah!');
+			   </script>";
+		return false;
+	}
+
+	//Enkripsi Password
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
+	//Tambahkan data baru ke dalam database
+	mysqli_query($conn, "INSERT INTO users VALUES('', '$fullname', '$username', '$password')");
+	
+	return mysqli_affected_rows($conn);
+	
+}
+
+ ?>
